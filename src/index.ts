c2600c941c9cb4d899387ba02e7117f170a2814b
QuantumChemist <c.zitlau@live.com>
2025-04-18 11:23:50 +0200

    extend the bot base
    Co-authored-by: fox3000foxy <fox3000foxy@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { generateText } from 'ai';
+import type { CoreMessage } from 'ai';
 import { groq } from '@ai-sdk/groq';
 import dotenv from 'dotenv';
 import { Client, Message, GatewayIntentBits } from 'discord.js';
@@ -8,19 +9,25 @@ const client: Client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
 });
 
-client.once('ready', () => {
-  console.log('Bot is online!');
-});
+const botLore: string = `You are a friendly assistant! You can answer questions, provide information, and assist users in a helpful manner. Your goal is to be as helpful as possible while maintaining a friendly demeanor.`;
 
 client.on('messageCreate', async (message: Message) => {
   if (message.author.bot) return;
   if (message.mentions.users.has(client.user?.id || '')) {
-    const prompt = message.content.replace(/<@!?\d+>/g, '').trim();
+    const messages = await message.channel.messages.fetch({ limit: 7 });
+    const prompt: CoreMessage[] = messages 
+      .filter(msg => !msg.author.bot || client.user?.id == msg.author.id)
+      .map(msg => ({
+        role: msg.author.id === message.author.id ? 'user' : 'assistant',
+        content: msg.content,
+      } as CoreMessage))
+      .reverse();
+    // const prompt = message.content.replace(/<@!?\d+>/g, '').trim();
     try {
       const { text }: {text: string} = await generateText({
         model: groq('llama-3.3-70b-versatile'),
-        system: 'You are a friendly assistant!',
-        prompt,
+        system: botLore,
+        messages: prompt,
       });
       message.reply(text);
     } catch (error: any) {
@@ -30,4 +37,9 @@ client.on('messageCreate', async (message: Message) => {
   }
 });
 
+client.once('ready', () => {
+  console.log('Bot is online!');
+});
+
 client.login(process.env.DISCORD_TOKEN);
+
